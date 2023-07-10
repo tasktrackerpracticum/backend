d64b0307d30b31a6cfbd31f7caa2abb6fb0d9e02
Anton Masyuk <anton.m.rus@gmail.com>
2023-07-10 18:06:53 +0500

    admin site for models updated. mock database updated, model str method updated

--- backend/tasks/admin.py
+++ backend/tasks/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from tasks.models import (
+    Comment, Organization, OrganizationUser, Project, ProjectUser, Subtask,
+    Task
+)
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+
+
+class OrganizationUserInline(admin.TabularInline):
+    model = OrganizationUser
+    ordering = ('role', 'user')
+
+
+class ProjectUserInline(admin.TabularInline):
+    model = ProjectUser
+    ordering = ('role', 'user')
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [
+        OrganizationUserInline,
+    ]
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        ProjectUserInline,
+    ]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+
+@admin.register(Subtask)
+class SubtaskAdmin(admin.ModelAdmin):
+    pass
