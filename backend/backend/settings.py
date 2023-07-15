976dd9f350b795d9b59df8c24a8b2f9b6cf7830b
Anton Masyuk <anton.m.rus@gmail.com>
2023-07-15 22:13:40 +0500

    cors headers added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django_filters',
     'phonenumber_field',
     'rest_framework.authtoken',
+    'corsheaders',
     'users.apps.UsersConfig',
     'tasks.apps.TasksConfig',
 ]
@@ -29,6 +30,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
