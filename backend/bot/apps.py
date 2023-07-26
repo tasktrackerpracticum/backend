9c1d25d934e5a9bc8e0182830b67620d6273344f
Sergey Afonin <afoninsb@yandex.ru>
2023-07-26 20:24:11 +0300

    bot app & Bot class

---
+++ backend/bot/apps.py
@@ -0,0 +1,23 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+
+class BotConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bot'
+
+    def ready(self):
+        from classes import Bot
+        bot = Bot(settings.BOT_TOKEN)
+        url = f'/webhook/{settings.BOT_TOKEN}/'
+        data = {
+            'url': f'{settings.BASE_URL}{url}'
+        }
+        bot.set_webhook(data)
+        bot.commands = [
+            {
+                'command': 'cancel',
+                'description': 'Отменить текущую операцию'
+            },
+        ]
+        bot.set_commands()
---
+++ backend/bot/classes.py
@@ -0,0 +1,93 @@
+"""Команды бота."""
+
+
+import contextlib
+import json
+import requests
+from typing import Any
+
+
+class Bot():
+
+    def __init__(
+            self,
+            token: str,
+            **kwargs,
+    ):
+        self.token = token
+        self.url = f'https://api.telegram.org/bot{self.token}/'
+
+    def user_id(self, data: dict[str, Any]) -> int:
+        """Получение chat id пользователя в Telegram.
+
+        Args:
+            data (dict): обновление, поступившее на вебхук.
+        Returns:
+            int: chat id пользователя в Telegram.
+        """
+        message_object = self.get_message(data)
+        if 'chat' in message_object:
+            return message_object['chat'].get('id', 0)
+        if 'from' in message_object:
+            return message_object['from'].get('id', 0)
+        return 0
+
+    @staticmethod
+    def get_message(data: dict[str, Any]) -> dict[str, Any] | None:
+        """Получение объекта message."""
+        if 'message' in data:
+            return data['message']
+        if 'callback_query' in data:
+            return data['callback_query'].get('message', {})
+        return {}
+
+    def send_answer(self, answer: dict[str, Any]) -> None:
+        """Отправка сообщения в бот."""
+        method = f'{self.url}sendMessage'
+        with contextlib.suppress(Exception):
+            requests.post(method, data=answer)
+
+    def send_photo(self, data: dict[str, Any]) -> None:
+        """Отправка изображения в бот."""
+        method = f'{self.url}sendPhoto'
+        with contextlib.suppress(Exception):
+            requests.post(method, data=data)
+
+    def delete_webhook(self) -> None:
+        """Удаление вебхука бота."""
+        method = f'{self.url}deleteWebhook'
+        with contextlib.suppress(Exception):
+            requests.post(method)
+
+    def set_webhook(self, data: dict[str, Any]) -> None:
+        """Установка веб-хука бота."""
+        method = f'{self.url}setWebhook'
+        with contextlib.suppress(Exception):
+            requests.post(method, data=data)
+
+    def set_commands(self) -> None:
+        """Установка комманд бота."""
+        method = f'{self.url}setMyCommands'
+        commands = json.dumps(self.commands)
+        send_text = f'{method}?commands={commands}'
+        with contextlib.suppress(Exception):
+            requests.get(send_text)
+
+    @staticmethod
+    def get_content_type(message: dict[str, Any]) -> str:
+        """Получение типа контента, поступвшего на вебхук."""
+        contents = ['text', 'document', 'photo']
+        return next(
+            (content for content in contents if content in message), '')
+
+    @staticmethod
+    def get_data_type(data: dict[str, Any]) -> str | None:
+        """Получение типа обновления, поступвшего на вебхук."""
+        if 'callback_query' in data:
+            return 'callback_query'
+        if 'message' in data and 'text' in data['message']:
+            return (
+                'command' if data['message']['text'].startswith('/')
+                else 'text'
+            )
+        return ''
