1460a79eeb27e2917b2dde4facea29284f1e65e5
Антон Масюк <37059480+catstyle1101@users.noreply.github.com>
2023-07-29 20:19:58 +0500

    Anton (#54)
    * readme change
    
    * base apu url changed
    
    * accounts changed endpoint address

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -25,10 +25,10 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('webhook/', include('bot.urls', namespace='bot')),
-    path('accounts/login/', LoginView.as_view(
+    path('api/accounts/login/', LoginView.as_view(
         template_name='admin/login.html',
     )),
-    path('accounts/logout/', LogoutView.as_view(
+    path('api/accounts/logout/', LogoutView.as_view(
         template_name='admin/logout.html',
     )),
     path('', include('djoser.urls.jwt')),
