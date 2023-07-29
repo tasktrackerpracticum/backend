9d6a385c76e0ba58fc353f72ad88d10956c98c8b
Sergey Afonin <afoninsb@yandex.ru>
2023-07-29 09:59:09 +0300

    change bot.classes.tguser

--- backend/bot/classes/tguser.py
+++ backend/bot/classes/tguser.py
@@ -4,8 +4,7 @@ from users.models import User
 class TgUser():
 
     def __init__(self, chat_id):
-        self.chat_id = chat_id
-        self.obj = User.objects.filter(chat_id=self.chat_id)
+        self.obj = User.objects.filter(chat_id=chat_id)
 
     def user_obj(self) -> int:
         """Получаем объект User из БД."""
