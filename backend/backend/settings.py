eea380ac98c15635852243ed9a052abedf4cbe30
catstyle1101 <anton.m.rus@gmail.com>
2023-07-31 09:58:10 +0500

    add last_login field update on any request

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -46,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'users.middlewares.SetLastVisitMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
