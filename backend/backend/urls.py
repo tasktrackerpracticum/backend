cdd50155fd067117cb2d39c95b8f1fd554336c6b
catstyle1101 <anton.m.rus@gmail.com>
2023-08-01 16:35:30 +0500

    schemas and README fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,8 +4,16 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from drf_yasg import openapi
+from drf_yasg.generators import OpenAPISchemaGenerator
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+
+
+class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
+    def get_schema(self, request=None, public=False):
+        schema = super().get_schema(request, public)
+        schema.schemes = ["http", "https"]
+        return schema
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,7 +27,8 @@ schema_view = get_schema_view(
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    generator_class=BothHttpAndHttpsSchemaGenerator,
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
