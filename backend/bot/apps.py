197aa49c09a1f31c125c53383e01a82538a8c1e2
Sergey Afonin <afoninsb@yandex.ru>
2023-08-01 20:27:21 +0300

    change bot.apps

--- backend/bot/apps.py
+++ backend/bot/apps.py
@@ -6,7 +6,7 @@ class BotConfig(AppConfig):
     name = 'bot'
 
     def ready(self):
-        from bot.classes.bot import bot
+        from bot.classes.bot import tgbot
         from bot.config import config
-        bot.set_webhook({'url': config.WEBHOOK_URL})
-        bot.set_commands()
+        tgbot.set_webhook({'url': config.WEBHOOK_URL})
+        tgbot.set_commands()
