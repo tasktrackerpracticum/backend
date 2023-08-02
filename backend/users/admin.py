2ebf6f3577285c61c647ac9109b22164f432712e
Sergey Afonin <afoninsb@yandex.ru>
2023-08-02 21:03:00 +0300

    chat_id in UserAdmin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -7,11 +7,14 @@ from users.models import User
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = (
-        'email', 'username', 'first_name', 'last_name', 'chat_id', 'phone', 'position',
-        'gender', 'is_staff', 'is_active', 'photo', 'country', 'timezone'
+        'email', 'username', 'first_name', 'last_name', 'chat_id', 'phone',
+        'position', 'gender', 'is_staff', 'is_active', 'photo', 'country',
+        'timezone'
     )
     search_fields = ('first_name', 'username', 'email', 'phone')
     list_display = (
-        'username', 'first_name', 'last_name', 'email', 'chat_id', 'phone', 'country')
+        'username', 'first_name', 'last_name', 'email', 'chat_id', 'phone',
+        'country'
+    )
     list_filter = ('country', 'is_active', 'is_staff')
     ordering = ('is_active', 'date_joined', 'username', 'email')
